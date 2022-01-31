f0c36241f393f6e74a5929a5b3011d848930eb5c
Safal <mr.safaladh@gmail.com>
2022-01-31 08:34:24 +0545

    Worked on Navbar logog

--- Cobuild/main.js
+++ Cobuild/main.js
@@ -8,4 +8,10 @@ window.addEventListener("scroll", function () {
     header.classList.toggle("scrolling-active", windowPosition);
     let logo = document.querySelector("img")
     windowPosition ? logo.src = ("images/logo-dark.png") : logo.src = ("images/logo-light.png");
+    // if (windowPosition == true) {
+    //     $(".nav img").attr("src", "images/logo-dark.png");
+    // }
+    // else {
+    //     $(".nav img").attr("src", "images/logo-light.png");
+    // }
 });
