871d2627d29a85aefa2ca88b48a3283f5f27c792
Safal <mr.safaladh@gmail.com>
2022-02-03 12:53:58 +0545

    Started working on Layerdrop

---
+++ Layerdrops/main.js
@@ -0,0 +1,17 @@
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    prevArrow: '<i class = "fas fa-angle-left  left_arrow"></i>',
+    nextArrow: '<i class = "fas fa-angle-right  right_arrow"></i>',
+    // mobileFirst: true,
+    // responsive: [{
+    //     breakpoint: 1024,
+    //     settings: {
+    //         slidesToShow: 1,
+    //         slidesToScroll: 1,
+    //         centerMode: false
+    //     }
+    // }]
+
+});
