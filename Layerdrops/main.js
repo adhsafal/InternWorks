2433f0e09d347a5ddf69fa896c2c932a5b059edd
Safal <mr.safaladh@gmail.com>
2022-02-03 21:18:37 +0545

    Working on navbar animation

--- Layerdrops/main.js
+++ Layerdrops/main.js
@@ -1,17 +1,26 @@
-$('.slider').slick({
-    infinite: true,
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    prevArrow: '<i class = "fas fa-angle-left  left_arrow"></i>',
-    nextArrow: '<i class = "fas fa-angle-right  right_arrow"></i>',
-    // mobileFirst: true,
-    // responsive: [{
-    //     breakpoint: 1024,
-    //     settings: {
-    //         slidesToShow: 1,
-    //         slidesToScroll: 1,
-    //         centerMode: false
-    //     }
-    // }]
 
+
+
+// $('.slider').slick({
+//     infinite: true,
+//     slidesToShow: 6,
+//     slidesToScroll: 1,
+//     prevArrow: '<i class = "fas fa-angle-left  left_arrow"></i>',
+//     nextArrow: '<i class = "fas fa-angle-right  right_arrow"></i>',
+//     // mobileFirst: true,
+//     // responsive: [{
+//     //     breakpoint: 1024,
+//     //     settings: {
+//     //         slidesToShow: 1,
+//     //         slidesToScroll: 1,
+//     //         centerMode: false
+//     //     }
+//     // }]
+
+// });
+
+window.addEventListener("scroll", function () {
+    let header = document.querySelector("nav");
+    let windowPosition = window.scrollY > 90;
+    header.classList.toggle("scrolling-active", windowPosition);
 });
